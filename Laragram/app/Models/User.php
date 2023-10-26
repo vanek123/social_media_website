11f07ad9ac897f7c28995ff5c45c10a84c137d67
vanek123 <vanjaaleskevic@mail.ru>
2023-10-26 04:27:39 +0300

    caching and mail

--- Laragram/app/Models/User.php
+++ Laragram/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Mail;
+use App\Mail\NewUserWelcomeMail;
 
 class User extends Authenticatable
 {
--- Laragram/routes/web.php
+++ Laragram/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Mail\NewUserWelcomeMail;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
