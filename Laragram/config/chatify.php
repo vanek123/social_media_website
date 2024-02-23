a8af766f319b030718caa38e675938bdffafc97b
vanja <vanjaaleskevic@mail.ru>
2024-02-23 17:14:38 +0200

    added Use Case Diagram

--- Laragram/config/chatify.php
+++ Laragram/config/chatify.php
@@ -27,7 +27,7 @@ return [
         'namespace' => env('CHATIFY_ROUTES_NAMESPACE', 'Chatify\Http\Controllers'),
     ],
     'api_routes' => [
-        'prefix' => env('CHATIFY_API_ROUTES_PREFIX', 'chat/api'),
+        'prefix' => env('CHATIFY_API_ROUTES_PREFIX', 'chatify/api'),
         'middleware' => env('CHATIFY_API_ROUTES_MIDDLEWARE', ['api']),
         'namespace' => env('CHATIFY_API_ROUTES_NAMESPACE', 'Chatify\Http\Controllers\Api'),
     ],
@@ -117,7 +117,7 @@ return [
     */
     'sounds' => [
         'enabled' => true,
-        'public_path' => 'sounds/chat',
+        'public_path' => 'sounds/chatify',
         'new_message' => 'new-message-sound.mp3',
     ]
 ];
--- Laragram/resources/views/layouts/app.blade.php
+++ Laragram/resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
                     <div><img src="/svg/freeCodeCampLogo.svg" class="logo pe-3"></div>
